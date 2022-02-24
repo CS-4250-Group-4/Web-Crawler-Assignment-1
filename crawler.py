4a4afd3998a4d7d7cc2441ad25e0a5bf2758ebe4
Krys <21113012+Krystade@users.noreply.github.com>
2022-02-23 17:45:02 -0800

    Merged in Bobby's robots and reports changes

--- crawler.py
+++ crawler.py
@@ -3,13 +3,18 @@ from bs4 import BeautifulSoup
 from typing import Counter
 from asyncio.windows_events import NULL
 
+
 report_info = []
+disallowed_url_arr = []
+seed_count = 0
 word_count = {}
+
+session = requests.Session()
 
 def crawl(seed):
     debug = True
     depth = 0
-    maxDepth = 200
+    maxDepth = 100
     visited = []
     #Prompt user for seed URL (hardcoded for now)
     #Check robots.txt for any restricted pages
@@ -27,21 +32,43 @@ def crawl(seed):
     #seed = "https://www.japscan.ws/"
     domain = seed.split("/")[2]
     queue.append(seed)
-    
+
+    session.headers.update({'Host': domain,
+                            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                            'Connection': 'keep-alive',
+                            'Pragma': 'no-cache',
+                            'Cache-Control': 'no-cache'})
 
     while((depth < maxDepth) or (len(queue) == 0)):
         depth += 1
+        num_outLinks = 0
+        currentUrl = queue.pop(0)
+        
         #Every 20 pages print show the depth in the console
         if(depth%20 == 0):
-            print("depth: " + str(depth))
+            print("depth: " + str(depth) + "/" + str(maxDepth))
             #Every 100 pages show the size of the queue
             if(depth%100 == 0):
                 print("Queue length: " + str(len(queue)))
-        current_url = queue.pop(0)
+        
         if(debug):
-            print("requesting: " + current_url)
-        visited.append(current_url)
-        page = requests.get(current_url)
+            print("requesting: " + currentUrl)
+        visited.append(currentUrl)
+
+        try:
+            page = session.get(currentUrl, timeout=5)
+        except requests.exceptions.Timeout:
+            num_try = 0
+            while(num_try < 5):
+                time.sleep(5)
+                page = session.get(currentUrl, timeout=25)
+                if(page is not NULL):
+                    break
+                num_try += 1
+        except requests.exceptions.TooManyRedirects:
+            print('Bad url')
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
 
         #Count word for each url that is going to be visited
         start_wordcount(page)
@@ -49,9 +76,10 @@ def crawl(seed):
         #time.sleep(.01)
         soup = BeautifulSoup(page.text, 'html.parser')
         outlinks = soup.find_all("a", href=True)
-        #call split on link for # and only check first half
-        #ex https://docs.python-requests.org/en/latest/#the-contributor-guide
-        #ignore #the-contributor-guide and just go to https://docs.python-requests.org/en/latest/
+        # call split on link for # and only check first half
+        # ex https://docs.python-requests.org/en/latest/#the-contributor-guide
+        # ignore #the-contributor-guide and just go to https://docs.python-requests.org/en/latest/
+        
         for tag in outlinks:
             link = tag["href"]
             #if the tag doesnt have an href skip it
@@ -63,6 +91,7 @@ def crawl(seed):
                 pass
             #ex of split: link.split("/") = ['https:', '', 'www.cpp.edu', 'index.shtml']
             elif link[0:4] == "http":
+                num_outLinks += 1
                 if(domain == link.split("/")[2]):
                     #print("adding " + link["href"])
                     if((link not in visited) and (link not in queue)):
@@ -78,8 +107,10 @@ def crawl(seed):
                 if(link[0] != "/"):
                     link = "/" + link
                 newLink = "https://" + domain + link
+                num_outLinks += 1
                 if((newLink not in visited) and (newLink not in queue)):
                     queue.append(newLink)
+        report_info.append([currentUrl, num_outLinks])
     
     #Get top 100 words with count and write to .csv file
     counter = Counter(word_count)
@@ -87,17 +118,14 @@ def crawl(seed):
     save_wordcount_csv(most_frequent)
     word_count.clear()
 
-    seed = 'exit'
-
     if (debug):
         for link in queue:
             print(link)
         print("\n\nVISITED\n")
         for link in visited:
             print(link)
-            #Sort using Counter
-        print()
-        print("Queue length: " + str(len(queue)) + "\tVisited length: " + str(len(visited)))
+        print("\nQueue length: " + str(len(queue)) +
+              "\tVisited length: " + str(len(visited)))
 
 def save_wordcount_csv(most_frequent):
     fields = ['Word', 'Count']
@@ -116,7 +144,7 @@ def start_wordcount(url):
     soup = BeautifulSoup(page.text, 'html.parser')
 
     #Get text from the page
-    for each_text in soup.findAll(text=True):
+    for each_text in soup.findAll(p):
         content = each_text
         words = content.lower().split()
         #Append it to the wordlist and then clean the words of all symbols
@@ -146,10 +174,34 @@ def create_dictionary(final_word_list):
         else:
             word_count[word] = 1
 
+def init_robot_info(link):
+    disallowed_url_arr.clear()
+    url = link + 'robots.txt'
+    robot_txt = session.get(url, timeout=5).text
+
+    robot_txt_lines = robot_txt.split('\n')
+    if(len(robot_txt_lines) == 0):
+        return
+
+    for line in robot_txt_lines:
+        line_arr = line.split(' ')
+        if(len(line_arr) > 1):
+            if((line_arr[0] == 'Disallow:') and (line_arr[1] is not NULL)):
+                disallowed_url_arr.append(line_arr[1])
+
+
+def isAllowed(link):
+    for text in disallowed_url_arr:
+        if(text in link):
+            return False
+    return True
+
+
 def save_report_csv():
+    global seed_count
     fields = ['URL', 'Number of outlinks']
     report_info.insert(0, fields)
-    filename = "report.csv"
+    filename = "report" + str(seed_count) + ".csv"
 
     # writing to csv file
     with open(filename, 'w', ) as csvfile:
